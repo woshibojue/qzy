0e072b315d4cb0e2bd887492744b4ed938354c6e
woshibojue <woshibojue@gmail.com>
2021-04-06 11:35:17 +0800

    git add

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -319,7 +319,7 @@ Page({
     });
   },
   //登录授权按钮
-  ongetUserInfo: function () {
+  ongetUserInfo: function (e) {
     // wx.showLoading({
     //   title: "登录中...",
     // });
@@ -343,5 +343,23 @@ Page({
     //     console.log("callFunction test result: ", res);
     //   },
     // });
+    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认
+    // 开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+    wx.getUserProfile({
+      desc: "用于完善会员资料", // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        console.log(res.userInfo);
+        // userInfo: res.userInfo,
+        // hasUserInfo: true,
+      },
+    });
+    wx.chooseAddress({
+      success: (res) => {
+        console.log(res);
+      },
+      fail: function (err) {
+        console.log(err);
+      },
+    });
   },
 });
